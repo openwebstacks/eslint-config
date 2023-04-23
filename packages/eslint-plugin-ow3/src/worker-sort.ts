e1ceffad61345fff19496cd1a6000f9f13bdf2b6
Chris <chrisbreuer93@gmail.com>
2023-04-22 18:20:41 -0700

    chore: use custom stacks unocss path

--- packages/eslint-plugin-ow3/src/worker-sort.ts
+++ packages/eslint-plugin-ow3/src/worker-sort.ts
@@ -1,11 +1,12 @@
-import { loadConfig } from '@unocss/config'
+import { uiPath } from '@stacksjs/path'
 import type { UnoGenerator } from '@unocss/core'
 import { createGenerator } from '@unocss/core'
+import { loadConfig } from '@unocss/config'
 import { runAsWorker } from 'synckit'
-import { sortRules } from './sort-rules'
+import { sortRules } from './utils'
 
 async function getGenerator() {
-  const { config, sources } = await loadConfig()
+  const { config, sources } = await loadConfig(process.cwd(), uiPath('unocss.ts'))
   if (!sources.length)
     throw new Error('[@ow3/eslint-plugin] No `uno.config.ts` file found. Please create a bug report.')
   return createGenerator(config)
